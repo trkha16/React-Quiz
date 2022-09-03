678abbb4a46b643f318b8bdff9cd8bd20cba80da
trkha16 <tbkha1609@gmail.com>
2022-09-03 17:29:23 +0700

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Difficulty, fetchQuizQuestions, QuestionState } from "./API";
+import QuestionCard from "./components/QuestionCard";
+
+export type AnswerObject = {
+    question: string;
+    answer: string;
+    correct: boolean;
+    correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loading, setLoading] = useState(false);
+    const [questions, setQuestions] = useState<QuestionState[]>([]);
+    const [number, setNumber] = useState(0);
+    const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(true);
+
+    const startTrivia = async () => {
+        setLoading(true);
+        setGameOver(false);
+
+        const newQuestions = await fetchQuizQuestions(
+            TOTAL_QUESTIONS,
+            Difficulty.EASY
+        );
+
+        setQuestions(newQuestions);
+        setScore(0);
+        setUserAnswers([]);
+        setNumber(0);
+        setLoading(false);
+    };
+
+    const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+        if (!gameOver) {
+            // Users answer
+            const answer = e.currentTarget.value;
+            // Check answer
+            const correct = questions[number].correct_answer === answer;
+            if (correct) setScore((prev) => prev + 1);
+            const answerObject = {
+                question: questions[number].question,
+                answer,
+                correct,
+                correctAnswer: questions[number].correct_answer,
+            };
+            setUserAnswers((prev) => [...prev, answerObject]);
+        }
+    };
+
+    const nextQuestion = () => {
+        const nextQuestion = number + 1;
+        if (nextQuestion === TOTAL_QUESTIONS) {
+            setGameOver(true);
+        } else {
+            setNumber(nextQuestion);
+        }
+    };
+    console.log("answer", userAnswers);
+
+    return (
+        <div className="wrapper">
+            <h1 className="heading-title">REACT QUIZ</h1>
+
+            {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+                <button className="start" onClick={startTrivia}>
+                    Start
+                </button>
+            ) : null}
+
+            {!gameOver ? <p className="score">Score: {score}</p> : null}
+
+            {loading ? <p>Loading Questions ...</p> : null}
+
+            {!loading && !gameOver && (
+                <QuestionCard
+                    questionNumber={number + 1}
+                    totalQuestions={TOTAL_QUESTIONS}
+                    question={questions[number].question}
+                    answers={questions[number].answers}
+                    userAnswer={userAnswers ? userAnswers[number] : undefined}
+                    callback={checkAnswer}
+                ></QuestionCard>
+            )}
+
+            {!gameOver &&
+            !loading &&
+            userAnswers.length === number + 1 &&
+            number !== TOTAL_QUESTIONS - 1 ? (
+                <button className="next" onClick={nextQuestion}>
+                    Next Question
+                </button>
+            ) : null}
+        </div>
+    );
 }
 
 export default App;
